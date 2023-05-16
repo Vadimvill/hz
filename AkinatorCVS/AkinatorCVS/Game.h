4955e27751075ceca40c03e164f14b118dd60b3c
Vadimvill <112726254+Vadimvill@users.noreply.github.com>
2023-05-16 23:35:32 +0300

    Delete AkinatorCVS directory

--- AkinatorCVS/AkinatorCVS/Game.h
+++
@@ -1,9 +0,0 @@
-#include "stdlib.h"
-#include "stdio.h"
-char isAnswer(struct Node* node);
-struct Node* game(struct Node* node, FILE* file);
-void log(FILE* file, char* string, int* i);
-int isNeedInt(int min, int max, int value);
-int setArraySize(int a, int max);
-FILE* copyFile();
-void freeTree(struct Node* root);
